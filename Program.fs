c48a77dc51dbe74085087850c22dee912cdd9fbf
Roland4444 <turbopollux@rambler.ru>
2024-11-04 15:03:16 +0400

    ready

--- Program.fs
+++ Program.fs
@@ -3,18 +3,28 @@
 let rec transformLst(input: List<int>, result: List<int>) =
     match input with
     | [] | [ _ ]-> result
-    | _ -> let h2 = input.Tail.Head
-           let lst = [input.Head * h2] 
-           transformLst((input.Tail), (result @ lst))  
+    | _ -> transformLst((input.Tail), (result @ [input.Head * input.Tail.Head]))  
+
+let calculatePositiveCounter(input: List<int>) = 
+    (List.filter(fun (z:int) -> z < 0) (transformLst(List.filter (fun (p:int) -> p <> 0) input, []))).Length
+
+let ListFromString(input: string) = 
+    let lstStr = input.Split ' ' 
+    List.map (fun f -> f |> int) (lstStr |> Array.toList)
+    
 
 [<EntryPoint>]
 let main args =
-    printfn "Hello from F#"
-    let list1 = [1;2;3;4;5]
-    printfn "list1.IsEmpty is %b" (list1.IsEmpty)
-    printfn "list1.Length is %d" (list1.Length)
-    printfn "list1.Head is %d" (list1.Head)
-    printfn "list1.Tail.Head is %d" (list1.Tail.Head)
-    printfn "list1.Tail.Tail.Head is %d" (list1.Tail.Tail.Head)
-    printfn "list1.Item(1) is %d" (list1.Item(1))
+    let lst1 = [-1; 0; 1]
+    let lst2 = [-2; -5; 10]
+    let lst3 = [11; 23; -5; 0]
+    let lst4 = [43; 0; 23; -2; -3; 4; -1]
+    let listLst = [lst1; lst2; lst3; lst4]
+    List.iter (fun p -> printfn "%d" (calculatePositiveCounter(p))) listLst 
+    let lst12 = "-1 0 1"
+    let lst22 = "-2 -5 10"
+    let lst32 = "11 23 -5 0"
+    let lst42 = "43 0 23 -2 -3 4 -1"
+    let listLst2 = [lst12; lst22; lst32; lst42]
+    List.iter (fun p -> printfn "%d" (calculatePositiveCounter(ListFromString(p)))) listLst2 
     0          
