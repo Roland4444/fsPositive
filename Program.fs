36a63a4341e616d8b07adea596a0500bd203345e
Roland4444 <turbopollux@rambler.ru>
2024-11-09 18:00:09 +0400

    simply

--- Program.fs
+++ Program.fs
@@ -1,6 +1,4 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-
-let rec transformLst(input: List<int>, result: List<int>) =
+﻿let rec transformLst(input: List<int>, result: List<int>) =
     match input with
     | [] | [ _ ]-> result
     | _ -> transformLst((input.Tail), (result @ [input.Head * input.Tail.Head]))  
@@ -9,21 +7,10 @@ let calculatePositiveCounter(input: List<int>) =
     (List.filter(fun (z:int) -> z < 0) (transformLst(List.filter (fun (p:int) -> p <> 0) input, []))).Length
 
 let ListFromString(input: string) = 
-    let lstStr = input.Split ' ' 
-    List.map (fun f -> f |> int) (lstStr |> Array.toList)    
+    List.map (fun f -> f |> int) (input.Split ' '  |> Array.toList)    
 
 [<EntryPoint>]
 let main args =
-    let lst1 = [-1; 0; 1]
-    let lst2 = [-2; -5; 10]
-    let lst3 = [11; 23; -5; 0]
-    let lst4 = [43; 0; 23; -2; -3; 4; -1]
-    let listLst = [lst1; lst2; lst3; lst4]
-    List.iter (fun p -> printfn "%d" (calculatePositiveCounter(p))) listLst 
-    let lst12 = "-1 0 1"
-    let lst22 = "-2 -5 10"
-    let lst32 = "11 23 -5 0"
-    let lst42 = "43 0 23 -2 -3 4 -1"
-    let listLst2 = [lst12; lst22; lst32; lst42]
+    let listLst2 = ["-1 0 1"; "-2 -5 10"; "11 23 -5 0"; "43 0 23 -2 -3 4 -1"]
     List.iter (fun p -> printfn "%d" (calculatePositiveCounter(ListFromString(p)))) listLst2 
     0          
